67c304bda058f59836f8d234d8162c37bc858fc1
Mihajlo7 <pav.mihajlo@gmail.com>
2023-04-01 17:34:43 +0200

    Implementirane su metode toString, hashCode i equals u klasama paketa artikli

--- Magacin/src/artikli/KucnaHemija.java
+++ Magacin/src/artikli/KucnaHemija.java
@@ -1,6 +1,7 @@
 package artikli;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class KucnaHemija extends Artikal {
 	public LocalDate getDatumTrajanja() {
@@ -12,4 +13,31 @@ public class KucnaHemija extends Artikal {
 	}
 
 	private LocalDate datumTrajanja;
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + Objects.hash(datumTrajanja);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		KucnaHemija other = (KucnaHemija) obj;
+		return Objects.equals(datumTrajanja, other.datumTrajanja);
+	}
+
+	@Override
+	public String toString() {
+		
+		return super.toString()+"KucnaHemija [datumTrajanja=" + datumTrajanja + "]";
+	}
+	
 }
