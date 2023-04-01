2b1b02c46c33532ab176b560fe002015bad1d3ee
Mihajlo7 <pav.mihajlo@gmail.com>
2023-04-01 17:56:15 +0200

    Dodata je logicka kontrola programa.

--- Magacin/src/artikli/Artikal.java
+++ Magacin/src/artikli/Artikal.java
@@ -11,24 +11,42 @@ public class Artikal {
 		return naziv;
 	}
 	public void setNaziv(String naziv) {
+		if(naziv==null) {
+			throw new IllegalArgumentException("naziv ne sme biti null");
+		}
+		if(naziv.isEmpty()) {
+			throw new IllegalArgumentException("naziv ne sme biti prazan string");
+		}
 		this.naziv = naziv;
 	}
 	public int getSifra() {
 		return sifra;
 	}
 	public void setSifra(int sifra) {
+		if(sifra>0) {
+			throw new IllegalArgumentException("Sifra mora biti veca od 0");
+		}
 		this.sifra = sifra;
 	}
 	public String getOpis() {
 		return opis;
 	}
 	public void setOpis(String opis) {
+		if(opis==null) {
+			throw new IllegalArgumentException("opis ne sme biti null");
+		}
+		if(opis.isEmpty()) {
+			throw new IllegalArgumentException("opis ne sme biti prazan string");
+		}
 		this.opis = opis;
 	}
 	public int getKolicina() {
 		return kolicina;
 	}
 	public void setKolicina(int kolicina) {
+		if(kolicina>0) {
+			throw new IllegalArgumentException("Kolicina mora biti veca od 0");
+		}
 		this.kolicina = kolicina;
 	}
 	@Override
